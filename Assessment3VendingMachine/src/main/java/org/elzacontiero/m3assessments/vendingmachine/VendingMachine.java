832391e954b6d632cd84f812580369aab3ed4c99
Elza Contiero <contieroelza@gmail.com>
2022-03-23 22:54:42 +0000

    throws NoItemInventoryException if index is invalid or quantity in stock is zero

--- Assessment3VendingMachine/src/main/java/org/elzacontiero/m3assessments/vendingmachine/VendingMachine.java
+++ Assessment3VendingMachine/src/main/java/org/elzacontiero/m3assessments/vendingmachine/VendingMachine.java
@@ -29,11 +29,19 @@ public class VendingMachine {
         return (int) (doubleMoney*100.0);
     }
 
-    private Item askUserForChoice() {
+
+    private Item askUserForChoice() throws NoItemInventoryException {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter your selection: ");
         int choice = Integer.parseInt(scan.nextLine()) - 1;
-        return inventory.get(choice);
+        if (choice<0 || choice>=inventory.size()) {
+            throw new NoItemInventoryException();
+        }
+        Item item = inventory.get(choice);
+        if (item.getQuantityAvailable() == 0) {
+            throw new NoItemInventoryException();
+        }
+        return item;
     }
 
     public void run() {
@@ -44,21 +52,34 @@ public class VendingMachine {
             presentMenu();
             int amount = askUserForMoney();
 
-            Item item = askUserForChoice();
-            System.out.println("Item selected: " + item.getName());
+            try {
+                Item item = askUserForChoice();
+                System.out.println("Item selected: " + item.getName());
 
-            while (item.getPrice() > amount) {
-                System.out.println("Insufficient funds. Your amount is: " + amount +
-                        ". Cost of item: " + item.getPrice() +
-                        ". Insert more money, please.");
-                amount += askUserForMoney();
+                /*
+                The specification says:
+                "If the user selects an item that costs more than the amount the user put into the vending machine,
+                the program should display a message indicating insufficient funds and then redisplay the amount
+                the user had put into the machine."
+                So I've implemented a loop that keeps asking for more money if the selected item costs more than
+                the available funds. The tiny loop rules out the need for any exceptions in it.
+                 */
+                while (item.getPrice() > amount) {
+                    System.out.println("Insufficient funds. Your amount is: " + amount +
+                            ". Cost of item: " + item.getPrice() +
+                            ". Insert more money, please.");
+                    amount += askUserForMoney();
+                }
+
+                amount -= item.getPrice();
+                Change change = new Change(amount);
+                System.out.println(change.toString());
+                item.setQuantityAvailable(item.getQuantityAvailable() - 1);
+                inventory.save();
             }
-
-            amount -= item.getPrice();
-            Change change = new Change(amount);
-            System.out.println(change.toString());
-            item.setQuantityAvailable(item.getQuantityAvailable() - 1);
-            inventory.save();
+            catch (NoItemInventoryException e) {
+                System.out.println("Selected item is not available in the inventory.");
+            }
         }
         catch (IOException e) {
             System.out.println("Error in program. Error: " + e.getMessage());
