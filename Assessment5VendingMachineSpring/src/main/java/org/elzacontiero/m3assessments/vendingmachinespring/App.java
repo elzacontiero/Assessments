a67dfd6bceb9016b73be6ed7b6c4af0eb4ddc98e
Elza Contiero <contieroelza@gmail.com>
2022-04-09 11:40:45 +0100

    Adding Assessment 5 Vending Machine console app.

---
+++ Assessment5VendingMachineSpring/src/main/java/org/elzacontiero/m3assessments/vendingmachinespring/App.java
@@ -0,0 +1,13 @@
+package org.elzacontiero.m3assessments.vendingmachinespring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
